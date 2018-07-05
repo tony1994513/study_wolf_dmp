bb76b299bce5912021c01c05c10bcacec8018b21
tony <470963660@qq.com>
2018-07-05 11:27:48 +0800

    cool

--- examples/moving_target_draw_number_2.py
+++ examples/moving_target_draw_number_2.py
@@ -21,35 +21,55 @@ import seaborn
 
 import pydmps
 import pydmps.dmp_discrete
+import baxter_interface
+import pickle
+import rospy
+from std_srvs.srv import Trigger,TriggerResponse
 
-y_des = np.load('2.npz')['arr_0'].T
-y_des -= y_des[:, 0][:, None]
+joint_cmd_names = [
+    'right_s0',
+    'right_s1',
+    'right_e0',
+    'right_e1',
+    'right_w0',
+    'right_w1',
+    'right_w2',
+]
+ending_angles = None
+def callback(req):
+    global ending_angles
+    resp = TriggerResponse()
+    resp.success = True
+    ending_angles = ending_angles = [0.603621440033, 0.680703974624, -0.0709466114397,
+                         1.06650014278, -0.00920388472731, 1.1780972451, -0.39883500485]
+    return resp
 
-# test normal run
-dmp = pydmps.dmp_discrete.DMPs_discrete(n_dmps=2, n_bfs=500, ay=np.ones(2)*10.0)
+def main():
+    rospy.init_node("Test_online_dmp")
+    trigger = rospy.Service('/task_change_flag', Trigger, callback)
+    limb_interface = baxter_interface.limb.Limb('right')
+    y_des = np.loadtxt("go_to_pick_position_dmp.txt")
+    y_des = y_des[:,1:8]
+    move_to_start_position = y_des[0]
+    limb_interface.move_to_joint_positions(dict(zip(joint_cmd_names, move_to_start_position)))
+    starting_angles = [limb_interface.joint_angle(joint) for joint in limb_interface.joint_names()]
 
-dmp.imitate_path(y_des=y_des)
+    dmp = pydmps.dmp_discrete.DMPs_discrete(n_dmps=7, n_bfs=100, ay=np.ones(7)*10.0)
 
-plt.figure(1, figsize=(6,6))
+    dmp.imitate_path(y_des=y_des.T)
+    dmp.y0 = np.array(starting_angles)
+    # y_track, dy_track, ddy_track = dmp.rollout()
+    dmp.reset_state()   
+    global ending_angles
+    for t in range(dmp.timesteps):
 
-y_track, dy_track, ddy_track = dmp.rollout()
-plt.plot(y_track[:,0], y_track[:, 1], 'b--', lw=2, alpha=.5)
+        y, _, _ = dmp.step()
+        print y
+        limb_interface.set_joint_positions(dict(zip(joint_cmd_names,y)))
+        rospy.sleep(0.2)
+        ending_angles = y_des[-1]
+        dmp.goal = ending_angles
+    print "Done"
 
-# run while moving the target up and to the right
-y_track = []
-dmp.reset_state()
-for t in range(dmp.timesteps):
-    y, _, _ = dmp.step()
-    y_track.append(np.copy(y))
-    # move the target slightly every time step
-    dmp.goal += np.array([1e-2, 1e-2])
-y_track = np.array(y_track)
-
-plt.plot(y_track[:,0], y_track[:, 1], 'b', lw=2)
-plt.title('DMP system - draw number 2')
-
-plt.axis('equal')
-plt.xlim([-2, 2])
-plt.ylim([-2, 2])
-plt.legend(['original path', 'moving target'])
-plt.show()
+if __name__ == '__main__':
+    main()
